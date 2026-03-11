c19ce99a04140c80194449fd547535415ef762f0
Tadhg Ledlie <tadhgsledlie@gmail.com>
2026-03-11 17:13:46 -0400

    update lab 1a.py test

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -6,7 +6,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 """
 
 def main():
-    print("Hello World!")
+    print("Hello All!")
 
     name = "Tadhg Ledlie" # TODO: Insert your name between the double quotes
 
